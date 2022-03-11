67407a149198f9051eeba23baf7cf935a28176a2
Stephen Broadus <stephenbroadus197@gmail.com>
2022-03-11 18:15:55 -0500

    Add files via upload

--- WhiteboardModel/src/com/game/client/NPCClient.java
+++ WhiteboardModel/src/com/game/client/NPCClient.java
@@ -7,8 +7,21 @@ public class NPCClient {
     public static void main(String[] args) {
         // create com.game.NPC object and set its properties
         NPC npcAvatar = new NPC();
+        System.out.println(npcAvatar);
+        NPC npcAvatar2 = new NPC("Zulu");
+        System.out.println(npcAvatar2);
+        NPC npcAvatar3 = new NPC("Zulu", 23);
+        System.out.println(npcAvatar3);
+
+        /*
         npcAvatar.setName("Zulu");
         System.out.println(npcAvatar.toString());
         npcAvatar.speak("There is no spoon");
+
+        NPC npcAvatar = new NPC();
+        System.out.println(npcAvatar);
+
+         */
+
     }
 }
